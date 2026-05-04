ca21be2a01ae1e4835d85934aca6f6d6e6adcea3
Driztaker <prom6381@gmail.com>
2026-05-04 10:33:37 +0100

    docker

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 /* --ainda por fazer*/
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Servidor a funcionar");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor na porta 3000");
+});
