6bfd927e3d969e05d77e122cf2054f0bb28438d2
goncalo756 <gfrodrigues.2006@gmail.com>
2026-05-04 16:36:55 +0100

    conexão há bd

--- app/base_de_dados.js
+++ app/base_de_dados.js
@@ -1,3 +1,15 @@
-//ainda por acabar
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const MONGO_URI = "mongodb://localhost:27017/votoseguro";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB ligado com sucesso");
+  } catch (error) {
+    console.error("Erro ao ligar MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+connectDB();
