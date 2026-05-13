041ef3b8d62d1751f91d982bd8bcf880f3852986
LA594 <ljbfabreu06@gmail.com>
2026-05-13 10:28:31 +0100

    Ainda não testei tudo, mas já tratei da maior parte dos erros de cripto, agora vou tratar do erro em que o browser dá aviso de não ser ssl, mas nós temos lol

--- servidor-ca/ca.py
+++ servidor-ca/ca.py
@@ -8,7 +8,10 @@ from cryptography import x509
 from cryptography.x509.oid import NameOID
 from dotenv import load_dotenv #para ser possivel buscar password apartir do .env
 from flask import Flask, request, jsonify
-from auth import generate_salt, hash_password, verify_password
+from criptografia.auth import generate_salt, hash_password, verify_password
+from criptografia.ECDH import geraracao_chaves, serializar_publica, deserializar_publica, calculo_chave_sessao
+from criptografia.RSA_voto import publica_serializada, verifica_assinatura
+from criptografia.AES_voto import encriptar, desencriptar
 
 load_dotenv() 
 
@@ -155,6 +158,58 @@ def sign():
     })
 
 
+@app.route('/dh/gerar-chaves', methods=['POST'])
+def dh_gerar_chaves():
+    priv,pub= geraracao_chaves()
+    pub_serializada=serializar_publica(pub)
+    return jsonify({'chave_publica':pub_serializada.decode('utf-8') })
+
+@app.route('/dh/chave-sessao', methods=['POST'])
+def dh_chave_sessao():
+    data=request.json
+    chave_pub_bytes=data['chave_publica_remota'].encode('utf-8')
+    chave_pub_remota=deserializar_publica(chave_pub_bytes)
+    priv,_=geraracao_chaves()
+    chave_session=calculo_chave_sessao(priv,chave_pub_remota)
+    return jsonify({'chave_sessao':base64.b64encode(chave_session).decode('utf-8')})
+
+@app.route('/rsa/integridade', methods=['POST'])
+def verificar_rsa():
+    data=request.json
+    chave_pub_bytes2=data['chave_publica'].encode('utf-8')
+    chave_pub2=publica_serializada(chave_pub_bytes2)
+    dados_bytes=data['dados'].encode('utf-8')
+    assinatura_bytes=base64.b64decode(data['assinatura'])
+    validar=verifica_assinatura(
+        chave_pub2,
+        dados_bytes,
+        assinatura_bytes
+    )
+    return jsonify({'valida': validar})
+
+@app.route('/aes/encriptar', methods=['POST'])
+def aes_encriptar():
+    data=request.json
+    chave=base64.b64decode(data['chave'])
+    voto=data['voto'].encode('utf-8')
+    data_associa=data['data_associada'].encode('utf-8')
+    iv, voto_cifrado, tag= encriptar(chave, voto, data_associa)
+    return jsonify({
+        'iv':base64.b64encode(iv).decode('utf-8'),
+        'voto_cifrado': base64.b64encode(voto_cifrado).decode('utf-8'),
+        'tag': base64.b64encode(tag).decode('utf-8')
+    })
+
+@app.route('/aes/desencriptar', methods=['POST'])
+def aes_desencriptar():
+    data=request.json
+    chave=base64.b64decode(data['chave'])
+    iv=base64.b64decode(data['iv'])
+    voto_cifrado=base64.b64decode(data['voto_cifrado'])
+    tag=base64.b64decode(data['tag'])
+    data_associa=data['data_associada'].encode('utf-8')
+    voto=desencriptar(chave,data_associa,iv,voto_cifrado,tag)
+    return jsonify({'voto': voto.decode('utf-8')})
 
 
 
@@ -210,7 +265,7 @@ if __name__ == "__main__":
 
 
 
-
+# o erro de gerar a chave RSA era que estávamos a chamer o ca-py e não o server.py, portanto  pus o código do server para aqui para ser mais fácil, em vez de estar a complicar
 
 #teste:
 #if __name__ == "__main__":
